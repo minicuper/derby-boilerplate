915ad264dca583505df72bafe30e03497839b5e7
Eric <eric.desu@gmail.com>
2014-06-17 23:18:54 +0600

    formating

--- src/server/server.js
+++ src/server/server.js
@@ -17,17 +17,17 @@ var favicon = require('serve-favicon');
 //Подключаем store
 var connectStore, sessionStore;
 if (process.env.REDIS_HOST) {
-    var redis = require('redis');
+  var redis = require('redis');
 
-    var redisClient;
-    redisClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
-    if (process.env.REDIS_PASSWORD) redisClient.auth(process.env.REDIS_PASSWORD);
+  var redisClient;
+  redisClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+  if (process.env.REDIS_PASSWORD) redisClient.auth(process.env.REDIS_PASSWORD);
 
-    connectStore = require('connect-redis')(session);
-    sessionStore = new connectStore({host: process.env.REDIS_HOST, port: process.env.REDIS_PORT, pass: process.env.REDIS_PASSWORD});
+  connectStore = require('connect-redis')(session);
+  sessionStore = new connectStore({host: process.env.REDIS_HOST, port: process.env.REDIS_PORT, pass: process.env.REDIS_PASSWORD});
 } else {
-    connectStore = require('connect-mongo')(session);
-    sessionStore = new connectStore({url: process.env.MONGO_URL});
+  connectStore = require('connect-mongo')(session);
+  sessionStore = new connectStore({url: process.env.MONGO_URL});
 }
 
 // Обработчик ошибок - я вынес его в отдельную папочку,
@@ -53,93 +53,93 @@ derby.use(require('racer-bundle'));
 
 exports.setup = function setup(app, options, cb) {
 
-    // Инициализируем подкючение к БД (здесь же обычно подключается еще и redis)
-    var store = derby.createStore({
-        db: liveDbMongo(process.env.MONGO_URL + '?auto_reconnect', {safe: true}),
-        redis: redisClient
-    });
+  // Инициализируем подкючение к БД (здесь же обычно подключается еще и redis)
+  var store = derby.createStore({
+    db: liveDbMongo(process.env.MONGO_URL + '?auto_reconnect', {safe: true}),
+    redis: redisClient
+  });
 
-    var publicDir = options.static || path.join(__dirname, '/../../public');
+  var publicDir = options.static || path.join(__dirname, '/../../public');
 
-    // Здесь приложение отдает свой "бандл"
-    // (т.е. здесь обрабатываются запросы к /derby/...)
-    store.on('bundle', function(browserify) {
-        // Add support for directly requiring coffeescript in browserify bundles
-        browserify.transform({global: true}, coffeeify);
+  // Здесь приложение отдает свой "бандл"
+  // (т.е. здесь обрабатываются запросы к /derby/...)
+  store.on('bundle', function (browserify) {
+    // Add support for directly requiring coffeescript in browserify bundles
+    browserify.transform({global: true}, coffeeify);
 
-        // HACK: In order to use non-complied coffee node modules, we register it
-        // as a global transform. However, the coffeeify transform needs to happen
-        // before the include-globals transform that browserify hard adds as the
-        // first trasform. This moves the first transform to the end as a total
-        // hack to get around this
-        var pack = browserify.pack;
-        browserify.pack = function(opts) {
-            var detectTransform = opts.globalTransform.shift();
-            opts.globalTransform.push(detectTransform);
-            return pack.apply(this, arguments);
-        };
-    });
+    // HACK: In order to use non-complied coffee node modules, we register it
+    // as a global transform. However, the coffeeify transform needs to happen
+    // before the include-globals transform that browserify hard adds as the
+    // first trasform. This moves the first transform to the end as a total
+    // hack to get around this
+    var pack = browserify.pack;
+    browserify.pack = function (opts) {
+      var detectTransform = opts.globalTransform.shift();
+      opts.globalTransform.push(detectTransform);
+      return pack.apply(this, arguments);
+    };
+  });
 
-    var expressApp = express()
-        .use(favicon(path.join(publicDir, '/images/favicon.ico')))
-        .use(compression());
+  var expressApp = express()
+    .use(favicon(path.join(publicDir, '/images/favicon.ico')))
+    .use(compression());
 
-    if (publicDir) {
-        if (Array.isArray(publicDir)) {
-            for (var i = 0; i < publicDir.length; i++) {
-                var o = publicDir[i];
-                expressApp.use(o.route, serveStatic(o.dir));
-            }
-        } else {
-            expressApp.use(serveStatic(publicDir));
-        }
+  if (publicDir) {
+    if (Array.isArray(publicDir)) {
+      for (var i = 0; i < publicDir.length; i++) {
+        var o = publicDir[i];
+        expressApp.use(o.route, serveStatic(o.dir));
+      }
+    } else {
+      expressApp.use(serveStatic(publicDir));
     }
+  }
 
-    // Здесь в бандл добавляется клиетский скрипт browserchannel,
-    // и возвращается middleware обрабатывающее клиентские сообщения
-    // (browserchannel основан на longpooling - т.е. здесь обрабатываются
-    // запросы по адресу /channel)
-    expressApp.use(racerBrowserChannel(store));
+  // Здесь в бандл добавляется клиетский скрипт browserchannel,
+  // и возвращается middleware обрабатывающее клиентские сообщения
+  // (browserchannel основан на longpooling - т.е. здесь обрабатываются
+  // запросы по адресу /channel)
+  expressApp.use(racerBrowserChannel(store));
 
-    // В req добавляется метод getModel, позволяющий обычным
-    // express-овским котроллерам читать и писать в БД см. createUserId
-    expressApp.use(store.modelMiddleware());
+  // В req добавляется метод getModel, позволяющий обычным
+  // express-овским котроллерам читать и писать в БД см. createUserId
+  expressApp.use(store.modelMiddleware());
 
-    expressApp.use(require('cookie-parser')(process.env.SESSION_COOKIE));
-    expressApp.use(session({
-        secret: process.env.SESSION_SECRET,
-        store: sessionStore
-    }));
+  expressApp.use(require('cookie-parser')(process.env.SESSION_COOKIE));
+  expressApp.use(session({
+    secret: process.env.SESSION_SECRET,
+    store: sessionStore
+  }));
 
-    expressApp.use(createUserId);
+  expressApp.use(createUserId);
 
-    // Здесь регистрируем контроллеры дерби-приложения,
-    // они будут срабатывать, когда пользователь будет брать страницы с сервера
-    expressApp.use(app.router());
+  // Здесь регистрируем контроллеры дерби-приложения,
+  // они будут срабатывать, когда пользователь будет брать страницы с сервера
+  expressApp.use(app.router());
 
-    // Если бы у на были обычные экспрессовские роуты - мы бы положили их СЮДА
+  // Если бы у на были обычные экспрессовские роуты - мы бы положили их СЮДА
 
-    // Маршрут по умолчанию - генерируем 404 ошибку
-    expressApp.all('*', function (req, res, next) {
-        next('404: ' + req.url);
-    });
+  // Маршрут по умолчанию - генерируем 404 ошибку
+  expressApp.all('*', function (req, res, next) {
+    next('404: ' + req.url);
+  });
 
-    // Обработчик ошибок
-    expressApp.use(midError());
+  // Обработчик ошибок
+  expressApp.use(midError());
 
-    app.writeScripts(store, publicDir, {extensions: ['.coffee']}, function(err) {
-        cb(err, expressApp);
-    });
+  app.writeScripts(store, publicDir, {extensions: ['.coffee']}, function (err) {
+    cb(err, expressApp);
+  });
 }
 
 // Пробрасываем id-юзера из сессии в модель дерби,
 // если в сессии id нет - генерим случайное
 function createUserId(req, res, next) {
-    var model = req.getModel();
-    var userId = req.session.userId;
-    if (!userId) userId = req.session.userId = model.id();
-    model.set('_session.userId', userId);
-    next();
+  var model = req.getModel();
+  var userId = req.session.userId;
+  if (!userId) userId = req.session.userId = model.id();
+  model.set('_session.userId', userId);
+  next();
 }
 
 
